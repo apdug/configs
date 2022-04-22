6db7148accc9bba6814578cbd91fceb8e03aeed0
apdug <jp.clk.com@gmail.com>
2022-04-22 18:46:22 -0300

    Update inituser.sh

--- inituser.sh
+++ inituser.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+
+if [ -f ~/.pyenv/bin/pyenv ]; then
+  curl https://pyenv.run | bash
+fi
+
+if [ -f ~/.pyenv/bin/pyenv ]; then
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+fi
+
+exec $SHELL
+
 mkdir -p /webapps/clkec/production
 mkdir -p /webapps/clkec/production/logs
 mkdir -p /webapps/clkec/production/socks
 cd /webapps/clkec/production
 git clone $REPO project
+
+cd project
+if [ -e .python-version ]; then
+    pyenv install -s `cat .python-version`
+    # poetry init -q
+    poetry env use `cat .python-version`
+    poetry config virtualenvs.create true --local
+    poetry config virtualenvs.in-project true --local
+    poetry install
+fi
+
