a271eb3221da5649a1ea39f895ca626022a9e36d
apdug <jp.clk.com@gmail.com>
2022-04-22 17:29:53 -0300

    Update inituser.sh

--- inituser.sh
+++ inituser.sh
@@ -5,7 +5,7 @@ useradd --system --gid webapps --shell /bin/bash --home /webapps/clkec clkec
 gpasswd -a clkec sudo
 mkdir -p /webapps/clkec
 chown clkec /webapps/clkec
-echo clkec:FPNzZqbi7lgZP38_hzXwBkTq*9n52$UAEXJAPt!stbN | chpasswd
+echo clkec:$USRPASSWORD | chpasswd
 rm -rf /etc/sudoers.d/clkec
 sh -c "echo 'clkec ALL=NOPASSWD: ALL' >> /etc/sudoers"
 touch /etc/sudoers.d/clkec
@@ -16,3 +16,9 @@ chmod 700 /webapps/clkec/.ssh
 chmod 600 /webapps/clkec/.ssh/authorized_keys
 chown clkec /webapps/clkec/.ssh
 chown clkec /webapps/clkec/.ssh/authorized_keys
+
+mkdir -p /webapps/clkec/production
+mkdir -p /webapps/clkec/production/logs
+mkdir -p /webapps/clkec/production/socks
+cd /webapps/clkec/production
+git clone $REPO project
