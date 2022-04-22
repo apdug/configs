32b93cd5aa5248193c0a5a630b4de40e017ea5ca
apdug <jp.clk.com@gmail.com>
2022-04-22 17:55:18 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -15,3 +15,21 @@ wget https://repos.insights.digitalocean.com/install.sh
 chmod +x install.sh
 ./install.sh
 
+# Iniciando usuario
+groupadd --system webapps
+mkdir -p /webapps/
+useradd --system --gid webapps --shell /bin/bash --home /webapps/clkec clkec
+gpasswd -a clkec sudo
+mkdir -p /webapps/clkec
+chown clkec /webapps/clkec
+echo clkec:$USRPASSWORD | chpasswd
+rm -rf /etc/sudoers.d/clkec
+sh -c "echo 'clkec ALL=NOPASSWD: ALL' >> /etc/sudoers"
+touch /etc/sudoers.d/clkec
+sh -c "echo 'clkec ALL=NOPASSWD: ALL' >> /etc/sudoers.d/clkec"
+mkdir -p /webapps/clkec/.ssh/
+cp /root/.ssh/authorized_keys /webapps/clkec/.ssh/authorized_keys
+chmod 700 /webapps/clkec/.ssh
+chmod 600 /webapps/clkec/.ssh/authorized_keys
+chown clkec /webapps/clkec/.ssh
+chown clkec /webapps/clkec/.ssh/authorized_keys
