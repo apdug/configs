17c0a83c983d355b4b8f78bab0a9fae77b3326d3
apdug <jp.clk.com@gmail.com>
2022-04-29 20:12:18 -0300

    Update inituser.sh

--- inituser.sh
+++ inituser.sh
@@ -21,12 +21,15 @@ fi
 cd project
 git pull
 git checkout clkec4_standardsync
+echo "START"
 if [ -f .python-version ]; then
+    echo "INSTALL PYTHON VERSION"
     pyenv install -s `cat .python-version`
     # poetry init -q
     poetry env use `cat .python-version`
     poetry config virtualenvs.create true --local
     poetry config virtualenvs.in-project true --local
 fi
+echo "END"
 poetry env use `cat .python-version`
 cd ~/production/project/  && ~/.poetry/bin/poetry install --no-dev && ~/.poetry/bin/poetry run pip install --upgrade pip
